4a57ae491c42370b7744158c6514675043ce2da4
basv1996 <bas.vugts@gmail.com>
2019-04-02 09:42:22 +0200

    theme setup basic without styling

---
+++ functions.php
@@ -0,0 +1,59 @@
+<?php
+//dit is de file voor hooks
+// de basics van deze file is om stylesheets en scripts toe te voegen
+// als we bootstrap willen gebruiken hier in laden
+
+function load_stylesheets()
+{
+    //load bootstrap stylesheet
+  wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css',
+  array(), false, 'all');
+  wp_enqueue_style('stylesheet');
+
+  //load custom / own css
+  wp_register_style('style', get_template_directory_uri() . '/style.css',
+  array(), false, 'all');
+  wp_enqueue_style('style');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+
+function include_jquery()
+{
+
+  wp_deregister_script('jquery');
+  wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.1.3.min.js', '', 1, true);
+}
+  add_action('wp_enqueue_scripts', 'include_jquery');
+
+
+function loadjs()
+{
+
+  wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true );
+  wp_enqueue_script('customjs');
+
+}
+add_action('wp_enqueue_scripts', 'loadjs');
+
+
+
+add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+
+
+register_nav_menus(
+array(
+
+  'top_menu' => __('Top Menu', 'theme'),
+  'footer_menu' => __('Footer Menu', 'theme')
+      )
+
+);
+
+add_image_size('smallest', 300, 300, true);
+add_image_size('largest', 800, 800, true);
+
+
+  ?>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+  <html>
+
+      <head>
+
+        <?php wp_head();?>
+
+
+      </head>
+
+    <body <?php body_class();?>>
+
+<header>
+
+  <div class="container">
+
+  <?php wp_nav_menu (
+
+    array(
+
+      'theme_location' => 'top-menu',
+      'menu_class' => 'navigation'
+    )
+  );?>
+
+</div>
+</header>
